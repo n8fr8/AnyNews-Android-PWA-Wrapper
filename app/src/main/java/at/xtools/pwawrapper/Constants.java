24e5fce37d341bbb576aee5a9cfe381c26c896c1
n8fr8 <nathan@guardianproject.info>
2020-01-14 13:55:46 -0500

    add defaut anynews URLs

--- app/src/main/java/at/xtools/pwawrapper/Constants.java
+++ app/src/main/java/at/xtools/pwawrapper/Constants.java
@@ -3,8 +3,8 @@ package at.xtools.pwawrapper;
 public class Constants {
     public Constants(){}
     // Root page
-    public static String WEBAPP_URL = "https://www.leasingrechnen.at/";
-    public static String WEBAPP_HOST = "leasingrechnen.at"; // used for checking Intent-URLs
+    public static String WEBAPP_URL = "https://n8fr8.github.io/anynews/";
+    public static String WEBAPP_HOST = "n8fr8.github.io"; // used for checking Intent-URLs
 
 	// User Agent tweaks
     public static boolean POSTFIX_USER_AGENT = true; // set to true to append USER_AGENT_POSTFIX to user agent
